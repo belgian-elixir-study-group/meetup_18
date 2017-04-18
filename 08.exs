998f5bea567e87dbcbd9300749f8fee5ea10a69f
Yuri Leikind <yuri.leikind@gmail.com>
2017-04-18 20:43:09 +0200

    swap 8 and 9

--- 08.exs
+++ 08.exs
@@ -1,60 +1,46 @@
-# When a process is killed with the kill signal, the exit reason is :killed, not :kill.
-# This is important, as sending a :kill process would unconditionally take down the whole link set
+# But...
+# There is one special reason even a process trapping exit signals cannot catch: :kill
 
-defmodule Parent do
+defmodule P do
   def start_link do
-    spawn(Parent, :init, [])
+    spawn(P, :init, [])
   end
 
   def init() do
     Process.flag(:trap_exit, true)
-    child_pid = Child.start_link
-    IO.puts(["child: pid: ", inspect(child_pid)])
-    loop(child_pid)
-  end
-
-  def loop(child_pid) do
-    receive do
-
-      {:EXIT, pid_that_died, reason} ->
-        IO.puts("   ================")
-        IO.puts("linked child process #{inspect(pid_that_died)} has exited abnormally because of:")
-        IO.inspect reason
-        IO.puts("   ================")
-
-        loop(child_pid)
-
-      {:get_child_pid, from} ->
-        send(from, {:child_pid, child_pid})
-        loop(child_pid)
-    end
-  end
-end
-
-defmodule Child do
-  def start_link do
-    spawn_link(Child, :loop, [])
+    loop()
   end
 
   def loop do
     receive do
-      _ -> loop()
+      any_message ->
+        IO.puts("   ================")
+        IO.puts "received a message"
+        IO.inspect(any_message)
+        IO.puts("   ================")
+        loop()
     end
   end
 end
 
-pid = Parent.start_link
+pid = P.start_link
+
 IO.puts("parent alive: #{Process.alive?(pid)}")
 
-send(pid, {:get_child_pid, self()})
+Process.exit(pid, :please_die)
 
 receive do
-  {:child_pid, child_pid} ->
-    IO.puts(["about to kill pid ", inspect(child_pid)])
-    Process.exit(child_pid, :kill)
+  after 10 -> :ok
 end
 
+IO.puts("parent alive: #{Process.alive?(pid)}")
+
+Process.exit(pid, :kill)   # {:EXIT, #PID<0.70.0>, :kill}
+
+# send(pid, {:EXIT, self(), :kill})
 
 receive do
-  after 100 -> :ok
+  after 10 -> :ok
 end
+
+IO.puts("parent alive: #{Process.alive?(pid)}")
--- 09.exs
+++ 09.exs
@@ -1,44 +1,61 @@
-# But...
-# There is one special reason even a process trapping exit signals cannot catch: :kill
+# When a process is killed with the kill signal, the exit reason is :killed, not :kill.
+# This is important, as sending a :kill process would unconditionally take down the whole link set
 
-defmodule P do
+defmodule Parent do
   def start_link do
-    spawn(P, :init, [])
+    spawn(Parent, :init, [])
   end
 
   def init() do
     Process.flag(:trap_exit, true)
-    loop()
+    child_pid = Child.start_link
+    IO.puts(["child: pid: ", inspect(child_pid)])
+    loop(child_pid)
   end
 
-  def loop do
+  def loop(child_pid) do
     receive do
-      any_message ->
+
+      {:EXIT, pid_that_died, reason} ->
         IO.puts("   ================")
-        IO.puts "received a message"
-        IO.inspect(any_message)
+        IO.puts("linked child process #{inspect(pid_that_died)} has exited abnormally because of:")
+        IO.inspect reason
         IO.puts("   ================")
-        loop()
+
+        loop(child_pid)
+
+      {:get_child_pid, from} ->
+        send(from, {:child_pid, child_pid})
+        loop(child_pid)
     end
   end
 end
 
-pid = P.start_link
+defmodule Child do
+  def start_link do
+    spawn_link(Child, :loop, [])
+  end
 
+  def loop do
+    receive do
+      _ -> loop()
+    end
+  end
+end
+
+pid = Parent.start_link
 IO.puts("parent alive: #{Process.alive?(pid)}")
 
-Process.exit(pid, :please_die)
+send(pid, {:get_child_pid, self()})
 
 receive do
-  after 10 -> :ok
+  {:child_pid, child_pid} ->
+    IO.puts(["about to kill pid ", inspect(child_pid)])
+    Process.exit(child_pid, :kill)
 end
 
 IO.puts("parent alive: #{Process.alive?(pid)}")
 
-Process.exit(pid, :kill)
-
 receive do
-  after 10 -> :ok
+  after 100 -> :ok
 end
-
-IO.puts("parent alive: #{Process.alive?(pid)}")
